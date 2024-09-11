f15281a07548db23b3bc0e54403863f9d96d9615
ramalek263448 <263448@student.pwr.edu.pl>
2024-09-11 22:51:29 +0200

    more_cristals

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -1,11 +1,18 @@
 "use client";
+import Image from "next/image";
 import React from 'react'
+import { Carousel, Card } from './ui/apple-cards-carousel';
 import { Vortex } from './ui/vortex';
 
 
 import { WavyBackground } from './ui/wavy-background'
 
 const Hero = () => {
+
+  const cards = data.map((card, index) => (
+    <Card key={card.src} card={card} index={index} />
+  ));
+
     return (
       <div className='pb-20 pt-36'>
         <WavyBackground className="max-w-4xl mx-auto pb-40" backgroundFill='lavenderblush'>
@@ -16,6 +23,13 @@ const Hero = () => {
                 Discover the stone that resonates with your soul.
             </p>
         </WavyBackground>
+        <div className=" pt-32 max-w-4xl mx-auto pb-40"> 
+        <h2 className="max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
+        Unlock the Power Within: Crystals for Balance, Healing, and Transformation.
+      </h2>
+      <Carousel items={cards} />
+
+        </div>
         <div className=" pt-32 max-w-4xl mx-auto pb-40">
         <Vortex
         backgroundColor="black"
@@ -40,5 +54,78 @@ const Hero = () => {
       </div>
     );
   }
+
+
+  const DummyContent = () => {
+    return (
+      <>
+        {[...new Array(3).fill(1)].map((_, index) => {
+          return (
+            <div
+              key={"dummy-content" + index}
+              className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4"
+            >
+              <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+                <span className="font-bold text-neutral-700 dark:text-neutral-200">
+                  The first rule of Apple club is that you boast about Apple club.
+                </span>{" "}
+                Keep a journal, quickly jot down a grocery list, and take amazing
+                class notes. Want to convert those notes to text? No problem.
+                Langotiya jeetu ka mara hua yaar is ready to capture every
+                thought.
+              </p>
+              <Image
+                src="https://assets.aceternity.com/macbook.png"
+                alt="Macbook mockup from Aceternity UI"
+                height="500"
+                width="500"
+                className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+              />
+            </div>
+          );
+        })}
+      </>
+    );
+  };
+   
+  const data = [
+    {
+      category: "Energetic Support",
+      title: "Crystals naturally emit vibrations that help harmonize the body and mind, promoting inner peace and balance.",
+      src: "https://images.unsplash.com/photo-1551232864-0b336dcdc028?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      content: <DummyContent />,
+    },
+    {
+      category: "Healing for Body and Soul",
+      title: "Many crystals, like amethyst or rose quartz, aid in emotional healing, stress reduction, and support physical recovery.",
+      src: "https://images.unsplash.com/photo-1576616604564-aac751caf9cd?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njd8fGNyeXN0YWx8ZW58MHx8MHx8fDA%3D",
+      content: <DummyContent />,
+    },
+    {
+      category: "Protection from Negative Energy",
+      title: "Crystals such as black tourmaline act as protective shields, deflecting negative energy and ensuring a sense of safety.",
+      src: "https://images.unsplash.com/photo-1627506584439-4365062a632a?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODh8fGNyeXN0YWx8ZW58MHx8MHx8fDA%3D",
+      content: <DummyContent />,
+    },
+   
+    {
+      category: "Enhance Intuition",
+      title: "Crystals can deepen your connection with your intuition and higher self, leading to greater awareness and more informed decision-making.",
+      src: "https://images.unsplash.com/photo-1679395283817-39f56e249ea6?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTg3fHxjcnlzdGFsfGVufDB8fDB8fHww",
+      content: <DummyContent />,
+    },
+    {
+      category: "Spiritual Growth",
+      title: "Working with crystals supports personal growth, helping you understand yourself on a deeper level and guiding your spiritual transformation in alignment with your soul’s unique energy.",
+      src: "https://images.unsplash.com/photo-1602025788761-07f57645b0a6?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTl8fGNyeXN0YWx8ZW58MHx8MHx8fDA%3D",
+      content: <DummyContent />,
+    },
+    {
+      category: "Manifesting Intentions",
+      title: "Crystals like citrine and clear quartz amplify your intentions, helping you manifest goals, desires, and positive changes in your life through focused energy and intention-setting.",
+      src: "https://images.unsplash.com/photo-1628799882124-9422c30f3587?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTU1fHxjcnlzdGFsfGVufDB8fDB8fHww",
+      content: <DummyContent />,
+    },
+  ];
 
 export default Hero
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'images.unsplash.com',
+            port: '',
+            pathname: '/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'assets.aceternity.com',
+            port: '',
+            pathname: '/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'source.unsplash.com',
+            port: '',
+            pathname: '/**',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
